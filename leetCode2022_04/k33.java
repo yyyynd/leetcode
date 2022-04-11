acc957472520a2aa306b436fa3bc97de5e0f2a50
yyyynd <menghy0523@qq.com>
2022-04-11 11:21:59 +0800

    2022.4.11

---
+++ leetCode2022_04/k33.java
@@ -0,0 +1,40 @@
+package leetcode.leetCode2022_04;
+
+/**
+ * 如
+ *
+ */
+public class k33 {
+    public static void main(String[] args) {
+        System.out.println(new k33().search(new int[]{1,3}, 0));
+    }
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            //前半部分有序,注意此处用小于等于
+            if (nums[left] <= nums[mid]) {
+                //target在前半部分
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
