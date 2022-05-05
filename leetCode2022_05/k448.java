eb083f39e873de0170717e7daf64b8cd66959a38
yyyynd <menghy0523@qq.com>
2022-05-05 10:36:04 +0800

    “2022.5.5”

---
+++ leetCode2022_05/k448.java
@@ -0,0 +1,22 @@
+package leetcode.leetCode2022_05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class k448 {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        for (int num : nums) {
+            int x = (num - 1) % n;
+            nums[x] += n;
+        }
+        List<Integer> ret = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) {
+                ret.add(i + 1);
+            }
+        }
+        return ret;
+
+    }
+}
